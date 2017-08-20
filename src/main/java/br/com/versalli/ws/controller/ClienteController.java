aeac647bae1d1e9c04a96692381b0ad354222a16
Alex Novais <alexnovais@gmail.com>
2017-08-19 21:15:29 -0300

    Foi desenvolvido o método de cadastrar e buscas, com seus respetivos endpoints

--- src/main/java/br/com/versalli/ws/controller/ClienteController.java
+++ src/main/java/br/com/versalli/ws/controller/ClienteController.java
@@ -1,14 +1,52 @@
 package br.com.versalli.ws.controller;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController	
+import br.com.versalli.ws.model.Cliente;
+
+@RestController
 public class ClienteController {
-	
-	@RequestMapping(method=RequestMethod.GET, value="/clientes")
-	public void buscar(){
-		System.out.println("Chamou /clientes!");
+
+	Map<Integer, Cliente> clientes = new HashMap<>();
+	Integer proximoId = 1;
+
+	// Métodos de Negócio
+	private Cliente cadastrar(Cliente cliente) {
+
+		cliente.setId(proximoId);
+		proximoId++;
+
+		clientes.put(cliente.getId(), cliente);
+
+		return cliente;
+	}
+
+	private Collection<Cliente> buscarTodos() {
+		return clientes.values();
+	}
+	// End points para comunicação Web/Servidor
+
+	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+
+		Cliente clienteCadastrado = cadastrar(cliente);
+		return new ResponseEntity<>(clienteCadastrado, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/clientes", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Cliente>> buscarTodosClientes() {
+
+		Collection<Cliente> clientesEncontrados = buscarTodos();
+		return new ResponseEntity<>(clientesEncontrados, HttpStatus.OK);
 	}
 }
--- src/main/java/br/com/versalli/ws/model/Cliente.java
+++ src/main/java/br/com/versalli/ws/model/Cliente.java
@@ -1,21 +1,24 @@
 package br.com.versalli.ws.model;
 
 public class Cliente {
-	
+
 	Integer id;
 	String nome;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
 }
